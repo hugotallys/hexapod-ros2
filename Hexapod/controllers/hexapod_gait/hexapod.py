a74cef10ad7babaf19cf6b29fc1cb9791283a13e
hugotallys <htmo@ic.ufal.br>
2023-04-02 17:37:41 -0300

    simplest gait implemented

--- Hexapod/controllers/hexapod_gait/hexapod.py
+++ Hexapod/controllers/hexapod_gait/hexapod.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 from controller import Supervisor  # type: ignore
-
+    
 
 class Hexapod:
 
-    def __init__(self) -> None:
+    w = np.pi
+    dt = 0.01
+    t = np.arange(0., 2*np.pi/w, dt)
+    q = np.zeros(shape=18)
+
+    def __init__(self, A, B, leg_phase) -> None:
         self.robot = Supervisor()
         self.timestep = int(self.robot.getBasicTimeStep())
 
@@ -28,6 +33,42 @@ class Hexapod:
 
         # for s in self.sensors:
         #     s.enable(self.timestep)
+        self.q1_init = 0.
+        self.q2_init = -1.
+        self.q3_init = 2.5
+
+        self.A = A
+        self.B = B
+        self.C = 0.5 * self.B
+
+        self.q1t = self.A * np.sin(self.w * self.t) + self.q1_init
+        self.q2t = self.B * np.clip(np.cos(self.w * self.t), -1., 0.) + self.q2_init
+        self.q3t = self.q3_init * np.ones_like(self.q2t) # self.C * np.clip(np.cos(self.w * self.t), -1., 0.) + self.q3_init
+
+        self.inc = 0
+        self.s = 1
+        self.k = [int(p / (self.w * self.dt)) for p in leg_phase]
+
+        self.amp = 1
+        self.legset = None
+
+    def move(self):
+        _amp = self.amp
+
+        for i in range(0, 16, 3):
+            if self.legset is not None:
+                _amp = -self.amp if i in self.legset else self.amp
+            j = (self.inc - self.k[int(i / 3)]) % len(self.t)
+            self.q[i] = _amp * self.q1t[j]
+            self.q[i+1] = self.q2t[j]
+            self.q[i+2] = self.q3t[j]
+            self.inc = (self.inc + self.s) % len(self.t)
+
+    def rotate(self, clockwise=True):
+        self.legset = [0, 6, 12] if clockwise else [3, 9, 15]
+
+    def straight(self):
+        self.legset = None
 
     @staticmethod
     def cross_product(a: np.ndarray, b: np.ndarray) -> np.ndarray:
@@ -58,6 +99,8 @@ class Hexapod:
     #     )
 
     def step(self):
+        self.move()
+        self.setJointPositions(self.q)
         return self.robot.step(self.timestep)
 
     def getTimeStep(self):
--- Hexapod/controllers/hexapod_gait/hexapod_gait.py
+++ Hexapod/controllers/hexapod_gait/hexapod_gait.py
@@ -2,68 +2,46 @@ import numpy as np
 
 from hexapod import Hexapod
 
-class Gait:
 
-  w = 2 * np.pi
-  dt = 0.01
-  t = np.arange(0., 2*np.pi/w, dt)
-  q = np.zeros(shape=18)
-
-  def __init__(self, A, B, leg_phase) -> None:
-
-    self.q1_init = 0.
-    self.q2_init = -1.
-    self.q3_init = 2.5
-
-    self.A = A
-    self.B = B
-    self.C = 0.5 * self.B
-
-    self.q1t = self.A * np.sin(self.w * self.t) + self.q1_init
-    self.q2t = self.B * np.clip(np.cos(self.w * self.t), -1., 0.) + self.q2_init
-    self.q3t = self.q3_init * np.ones_like(self.q2t) # self.C * np.clip(np.cos(self.w * self.t), -1., 0.) + self.q3_init
-
-    self.inc = 0
-    self.s = 1
-    self.k = [int(p / (self.w * self.dt)) for p in leg_phase]
-
-  def move(self):
-    
-    for i in range(0, 16, 3):
-      j = (self.inc - self.k[int(i / 3)]) % len(self.t)
-      self.q[i] = self.q1t[j]
-      self.q[i+1] = self.q2t[j]
-      self.q[i+2] = self.q3t[j]
-      self.inc = (self.inc + self.s) % len(self.t)
 
 if __name__ == "__main__":
 
-    # Tripod gait
+    # Tripod robot
     # A=.3, B=.15, q_init=[0., -1., 2.5], leg_phase=[0., np.pi, np.pi, 0., 0., np.pi]
     
-    robot = Hexapod()
-    gait = Gait(
-      A=.3, B=.15, leg_phase=[0., np.pi, np.pi, 0., 0., np.pi]
+    robot = Hexapod(
+       A=.3, B=.2, leg_phase=[0., np.pi, np.pi, 0., 0., np.pi]
     )
 
     # Initial joint configuration
     q0 = np.zeros(shape=18)
     
     for i in range(1, 18, 3):
-        q0[i] = 2*gait.q2_init
-        q0[i+1] = gait.q3_init
+        q0[i] = 2*robot.q2_init
+        q0[i+1] = robot.q3_init
         
     robot.setJointPositions(q0)
     robot.delay(500)
 
     for i in range(1, 18, 3):
-        q0[i] = gait.q2_init
-        q0[i+1] = gait.q3_init
+        q0[i] = robot.q2_init
+        q0[i+1] = robot.q3_init
     
     robot.setJointPositions(q0)
     robot.delay(500)
 
+    g = 0
+    gait = np.random.randint(0, 3, size=100)
+
     while robot.step() != -1:
-      gait.move()
-      robot.setJointPositions(gait.q)
-      robot.delay(5)
+
+      if gait[g] == 0:
+        robot.straight()
+      elif gait[g] == 1:
+        robot.rotate(clockwise=True)
+      elif gait[g] == 2:
+        robot.rotate(clockwise=False)
+      
+      robot.delay(2000)
+
+      g = (g + 1) % len(gait)
